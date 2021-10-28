22eadf5cb4af837f084f6d6fddbbdca437e5fff4
Joshua <44485177+j1m6h@users.noreply.github.com>
2021-10-28 00:35:20 -0500

    Update dldotf.sh

--- dldotf.sh
+++ dldotf.sh
@@ -5,4 +5,5 @@ curl https://raw.githubusercontent.com/j1m6h/dotfiles/main/.xinitrc > ~/.xinitrc
 curl https://raw.githubusercontent.com/j1m6h/dotfiles/main/.bashrc > ~/.bashrc
 curl https://raw.githubusercontent.com/j1m6h/dotfiles/main/.bash_profile > ~/.bash_profile
 curl https://raw.githubusercontent.com/j1m6h/dotfiles/main/.config/nvim/init.vim > ~/.config/nvim/init.vim
+curl https://raw.githubusercontent.com/j1m6h/dotfiles/main/.screenlayout/monitor_orientation.sh > ~/.screenlayout/monitor_orientation.sh
 echo "Finished."
