27ac846dd9682a5fca0595a6a8e16608d260d8d9
Joshua <44485177+j1m6h@users.noreply.github.com>
2021-10-28 01:47:54 -0500

    Update voidinst.sh

--- voidinst.sh
+++ voidinst.sh
@@ -6,15 +6,15 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 echo "Gathering deps..."
-xbps-install -S base-devel libX11-devel libXft-devel libXinerama-devel git picom feh lsd
+xbps-install -S xorg base-devel libX11-devel libXft-devel libXinerama-devel git picom feh lsd
 
 outputdir="/usr/local/src/"
 mkdir /usr/local/src
 
-git clone https://github.com/j1m6h/dwm $outputdir
-git clone https://github.com/j1m6h/st $outputdir
-git clone https://github.com/j1m6h/dmenu $outputdir
-git clone https://github.com/j1m6h/dwmblocks $outputdir
+git clone https://github.com/j1m6h/dwm $outputdir/dwm
+git clone https://github.com/j1m6h/st $outputdir/st
+git clone https://github.com/j1m6h/dmenu $outputdir/dmenu
+git clone https://github.com/j1m6h/dwmblocks $outputdir/dwmblocks
 
 cd /usr/local/src/dwm && make clean install 
 cd ../st && make clean install
